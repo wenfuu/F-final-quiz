67885ede0d0ab691d70cd55b76807d1fff8365ef
Du Juan <jdu.xian@gmail.com>
2020-10-30 12:34:29 +0800

    feedback by Du Juan

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import './style/reboot.scss';
 
+// TODO GTB-综合: * 完成度太低，知识点运用和工程实践没法验证
+// TODO GTB-完成度: * 功能没有完成
+// TODO GTB-知识点: * flex的使用基本掌握
 ReactDOM.render(<App />, document.getElementById('root'));
